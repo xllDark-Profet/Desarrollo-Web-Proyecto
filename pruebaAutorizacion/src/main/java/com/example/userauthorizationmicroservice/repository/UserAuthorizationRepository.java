c6eede8cc8ff32f02a491d7ee1c309776b5f9377
Andres Porras <anripoes@gmail.com>
2022-10-17 17:36:18 -0500

    modificacion del back

--- pruebaAutorizacion/src/main/java/com/example/userauthorizationmicroservice/repository/UserAuthorizationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.userauthorizationmicroservice.repository;
-
-import com.example.userauthorizationmicroservice.entity.UserAuthorization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAuthorizationRepository extends JpaRepository<UserAuthorization, Long> {
-    UserAuthorization findByUsername(String username);
-}

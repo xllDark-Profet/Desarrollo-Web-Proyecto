9d57d42a246b01c27fcf6e1f81c635eab4115034
Andres Porras <anripoes@gmail.com>
2022-11-15 12:32:30 -0500

    limpieza

--- FRONTEND/proyecto-bookstore/src/app/models/login-user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LoginUser } from './login-user';
-
-describe('LoginUser', () => {
-  it('should create an instance', () => {
-    expect(new LoginUser()).toBeTruthy();
-  });
-});
--- FRONTEND/proyecto-bookstore/src/app/models/new-user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { NewUser } from './new-user';
-
-describe('NewUser', () => {
-  it('should create an instance', () => {
-    expect(new NewUser()).toBeTruthy();
-  });
-});
